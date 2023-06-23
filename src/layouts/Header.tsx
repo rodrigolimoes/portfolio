7d217826d969ffa8e250c00edff4577de21a1d06
Rodrigo Limões <rolimoes@gmail.com>
2023-06-23 20:59:30 -0300

    feat: Adding Header

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from 'react';
+import { styled } from 'styled-components';
+
+const Header = styled.header`
+	display: flex;
+	padding: 20px;
+	justify-content: space-between;
+`;
+
+const ListItem = styled.ul`
+	display: flex;
+	justify-content: space-evenly;
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	& li {
+		padding: 0 10px;
+	}
+`;
+
+interface HeaderAppStateProps {}
+interface HeaderAppDispatchProps {}
+
+type HeaderAppProps = HeaderAppStateProps & HeaderAppDispatchProps;
+
+const HeaderApp: FC<HeaderAppProps> = () => {
+	return (
+		<Header>
+			<div>{`<RL/>`}</div>
+			<ListItem>
+				<li>Home</li>
+				<li>About</li>
+				<li>Projetos</li>
+			</ListItem>
+		</Header>
+	);
+};
+
+export default HeaderApp;
