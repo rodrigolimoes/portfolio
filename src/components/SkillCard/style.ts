29d16dee2d5937f9232a67c28abbf055e2d4825b
Rodrigo Limões <rolimoes@gmail.com>
2023-06-27 17:41:44 -0300

    feat: Adding SkillCard

---
+++ src/components/SkillCard/style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const SkillBox = styled.div`
+	width: 350px;
+	height: 200px;
+`;
+
+export const Title = styled.div`
+	display: flex;
+	align-items: center;
+
+	img {
+		width: 25px;
+		height: 25px;
+		margin-left: 5px;
+		object-fit: contain;
+	}
+
+	h4 {
+		display: inline-block;
+	}
+`;
