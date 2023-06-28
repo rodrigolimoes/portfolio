cc18283806198df8441c6cb207adde18774c78ae
Rodrigo Limões <rolimoes@gmail.com>
2023-06-28 00:16:41 -0300

    feat: Adding media query in Skill Style

--- src/components/SkillCard/style.ts
+++ src/components/SkillCard/style.ts
@@ -3,6 +3,18 @@ import styled from 'styled-components';
 export const SkillBox = styled.div`
 	width: 350px;
 	height: 200px;
+
+	@media only screen and (max-width: 1200px) and (min-width: 1024px) {
+		width: 350px;
+	}
+
+	@media only screen and (max-width: 1024px) {
+		width: 350px;
+	}
+
+	@media only screen and (max-width: 768px) {
+		width: 100%;
+	}
 `;
 
 export const Title = styled.div`
