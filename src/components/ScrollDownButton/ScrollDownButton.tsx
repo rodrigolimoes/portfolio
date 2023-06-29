bdd1d92ae9b3fe4710195413934f72f0221e8d01
Rodrigo Limões <rolimoes@gmail.com>
2023-06-28 23:47:52 -0300

    refactor: Changing type of href prop

--- src/components/ScrollDownButton/ScrollDownButton.tsx
+++ src/components/ScrollDownButton/ScrollDownButton.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
 import { ScrollDownButton as ScrollDownInputButton } from './style';
 
-interface ScrollButtonStateProps {}
+interface ScrollButtonStateProps {
+	href?: string;
+}
 interface ScrollButtonDispatchProps {}
 
 type ScrollButtonProps = ScrollButtonStateProps & ScrollButtonDispatchProps;
 
-const ScrollDownButton: FC<ScrollButtonProps> = () => {
-	return <ScrollDownInputButton />;
+const ScrollDownButton: FC<ScrollButtonProps> = ({ href }) => {
+	return <ScrollDownInputButton href={href} />;
 };
 
 export default ScrollDownButton;
